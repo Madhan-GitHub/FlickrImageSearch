28e574cee7872c9cdb87b47aab4cd5e74d74fbb7
Madhan <mailtomadhan.thangavel@gmail.com>
2025-02-13 15:06:55 -0600

    Initial Commit

---
+++ FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchApp.swift
+//  FlickrImageSearch
+//
+//  Created by Maddy on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickrImageSearchTests/FlickrImageSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchTests.swift
+//  FlickrImageSearchTests
+//
+//  Created by Maddy on 2/13/25.
+//
+
+import Testing
+@testable import FlickrImageSearch
+
+struct FlickrImageSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
